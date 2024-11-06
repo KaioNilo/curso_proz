5a26b4e1ffc253a34093434686ff194e45a6a8fa
Kaio Nilo <kaionilofreitas@gmail.com>
2024-11-06 20:09:55 -0300

    ajustando ativ

--- Aulas/Javascrip1/script.js
+++ Aulas/Javascrip1/script.js
@@ -1,8 +1,8 @@
 function msg() {
-    for(let i = 0; i < 4; i++) {
-        console.log('Conexão feita com sucesso!');
-    }
+    console.log('Conexão feita com sucesso!');
 } 
 
+console.log(msg())
+console.log(msg())
 console.log(msg())
 
